2eb95c0189128c6f346004c58ddefe5c33079fab
XVS9000 <123215782+XVS9000@users.noreply.github.com>
2023-01-23 11:07:18 -0500

    meow

--- HomePage.php
+++ HomePage.php
@@ -1,6 +1,7 @@
 
 <html>
     <head>
+        <!--- meta data --->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Bootstrap :3</title>
@@ -8,17 +9,32 @@
     </head>
 
     <body>
+        <!--- website --->
         <title>
             Home Page
         </title>
 
-        <header>
-            Volenteer
-        </header>
+        <h2><center>the home page </center></h2>
 
-        <header> 
-        </header>  
+        <h3>
+            Volunteer
+        </h3>
+
+        <h3> 
+            Profile
+        </h3>
+
+        <h3>
+            Missions in your location.
+        </h3>
         
+        <d1>
+        </d1>
+
+        <ul>
+
+        /ul>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
 
