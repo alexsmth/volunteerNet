c9678410f6b2efeea07bf5d441bf05a5bb2093f8
XVS9000 <123215782+XVS9000@users.noreply.github.com>
2023-01-25 23:34:35 -0500

    Website changes and added an image folder for site pictures like the favicon.

--- app/views/HomePage.php
+++ app/views/HomePage.php
@@ -4,27 +4,33 @@
         <!--- meta data --->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Bootstrap :3</title>
+        <title>VolunteerNet</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link rel="icon" type="image/x-icon" href="/images/temppic.png">
     </head>
 
     <body>
         <!--- website --->
-        <title>
-            Home Page
-        </title>
+        <div id="HomePage">
+            <header >the home page</header>
 
-        <div>
-            <header>the home page</header>
+            <hr allign="center"/>
+            <table>
+                <col>
+                    <h3>
+                    <a href="" target="_self">Volunteer</a>
+                    </h3>
+                </col>
 
-            <br></br>
-            <h3>
-                <a href="" target="_self">Volunteer</a>
-            </h3>
+                <col>
+                    <h3> 
+                        <a href = "" target = "_self"> Profile </a>
+                    </h3>
+                </col>
+            </table>
+            
 
-            <h3> 
-                <a href = "" target = "_self"> Profile </a>
-            </h3>
+            
 
             <article class="volunteer_opportunities">
                 <h3>
