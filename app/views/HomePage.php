ff6731540cd0977395c6361a8f716c96b66e6b83
XVS9000 <123215782+XVS9000@users.noreply.github.com>
2023-01-26 21:58:25 -0500

    developing the page for volunteer work

--- app/views/HomePage.php
+++ app/views/HomePage.php
@@ -19,10 +19,9 @@
     <body>
         <!--- home page website --->
         <div id="HomePage" align = "center">
-            <h1>the home page</h1>
-
+            <h1>Welcome to the VolunteerNet!</h1>
             <hr/>
-            <table>
+            <table class="homepagebuttons">
                 <th>
                     <h3 class="homepagebuttons">
                         <a href="" target="_self">Volunteer</a>
@@ -39,15 +38,17 @@
 
             
 
-            <article id="volunteer_opportunities">
-                <h3>
+            
+                <h3 align = "center">
                     Missions in your location.
                 </h3>
 
-                <div>
-
+                <div style=" border-block: double; block-size: 200px;">
+                    <h5 style="background-color: green; color: rgb(255, 255, 255);"> What's new in your area.<h5>
+                    <iframe src="" height="200" width="300" title="poop time">
+                    </iframe>
                 </div>
-            </article>
+            
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
