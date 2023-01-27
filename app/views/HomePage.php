5ad19ac91e029bed0992a3ad1ca22814edd6b94d
XVS9000 <123215782+XVS9000@users.noreply.github.com>
2023-01-27 11:01:54 -0500

    dsaasda

--- app/views/HomePage.php
+++ app/views/HomePage.php
@@ -6,15 +6,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>VolunteerNet</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <link rel="icon" type="image/x-icon" href="views/images/temppic.png">
+        <link rel="icon" type="image/x-icon" src="temppic.png">
 
         <style>
-            .homepagebuttons: {
-                 border: 1px;
-                 margin: 0px;
-                 padding-right: 10px;
-                 border-spacing: 20px;
-                 border-collapse: expression('separate', cellSpacing='10px');
+            .homepagebuttons{
+                padding: 10px;
+                margin-right: 5px;
             }
         </style>
     </head>
@@ -23,43 +20,38 @@
         <!--- home page website --->
         <div id="HomePage" align = "center">
             <h1 style="padding: 20px;">Welcome to the VolunteerNet!</h1>
-            <hr/>
+            <p>description</p>
 
-            <div id = "buttondivide" align = "center">
-                <table class="homepagebuttons">
+            <div id = "buttondivide" align = "right">
+                <table style="position: absolute; top: 0; right: 0;">
                     <th>
-                        <h3 align = "left">
-                            <a href="" target="_self">Volunteer</a>
-                        </h3>
+                        <button class="homepagebuttons" align="center" target="">Volunteer</button>
                     </th>
 
                     <th>
-                        <h3 align = "center"> 
-                            <a href = "" target = "_self"> Profile </a>
-                        </h3>
+                        <button class="homepagebuttons" align="center" target="">Profile</button>
                     </th>
 
-                    <br>
-                    <br>
-                    
                     <th>
-                        <h3 align = "right"> 
-                            <a href = "" target = "_self"> Ask for help </a>
-                        </h3>
+                        <button class="homepagebuttons" align="center" target="">Ask for help</button>
                      </th>
                 </table>
             </div>
+            <hr/>
+            <br>
+            <br>
 
             <h3 align = "center">
                 Missions in your location.
             </h3>
 
-            <div style=" border-block: double; height: 500px; width: 400px;">
+            <div style=" border-block: double; height: 900px; width: 1000px; padding: 50px;">
                 <h5 style="background-color: green; color: rgb(255, 255, 255);"> What's new in your area.<h5>
-                <iframe src="" height="500px" width="300px" title="poop time">
-                </iframe>
+                <iframe src="https://www.instagram.com/fdgvfdgd/?hl=en" style="padding: 50px;" height="900px" width="1000px" title="poop time"></iframe>
              </div>
-            
+
+             <br>
+             <br>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
