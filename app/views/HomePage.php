4ee77800db2886e789f29ec190a823f478c2a6b7
XVS9000 <123215782+XVS9000@users.noreply.github.com>
2023-01-26 22:11:06 -0500

    help

--- app/views/HomePage.php
+++ app/views/HomePage.php
@@ -22,7 +22,7 @@
             <h1>Welcome to the VolunteerNet!</h1>
             <hr/>
 
-            <div id = "buttondivide" align = "center" style = "block-size: 3px;">
+            <div id = "buttondivide" align = "center" style = "height: 10px; width: 20;">
                 <h3 align = "left" class="homepagebuttons">
                    <a href="" target="_self">Volunteer</a>
                 </h3>
