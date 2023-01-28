01cb7429a8526162826c77785d3f32c908d4eab1
XVS9000 <123215782+XVS9000@users.noreply.github.com>
2023-01-28 17:06:13 -0500

    fixing things

--- volunteerNet/app/controllers/LoginController.php
+++ volunteerNet/app/controllers/LoginController.php
@@ -1,6 +1,7 @@
 <?php
     class ProfileController extends SecureController {
+
         function index() {//temporary may need to add more may be fine for demonstration
-            $this->render_view("Profile.php", "main_layout.php");
+            $this->render_view('Login.php');
         }
     }
