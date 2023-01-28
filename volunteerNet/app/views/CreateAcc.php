ea57e8788d9cf427434ba84e26e283cd020d4f42
XVS9000 <123215782+XVS9000@users.noreply.github.com>
2023-01-28 01:04:34 -0500

    Create account webpage

--- volunteerNet/app/views/CreateAcc.php
+++ volunteerNet/app/views/CreateAcc.php
@@ -18,9 +18,54 @@
     </head>
 
     <body>
-        <!--- meow --->
-        <form action="link to the account data base" method="post"></form>
+        <!--- The little buttons --->
+        <div id = "buttondivide" align = "right">
+                <table style="position: absolute; top: 0; right: 0;">
+                    <th>
+                        <button class="homepagebuttons" align="center" action = "" target="_self">Volunteer</button>
+                    </th>
 
+                    <th>
+                        <button class="homepagebuttons" align="center" action = "" target="_self">Home</button>
+                    </th>
+
+                    <th>
+                        <button class="homepagebuttons" align="center" target="_self">Profile</button>
+                    </th>
+
+                    <th>
+                        <button class="homepagebuttons" align="center" target="_self">Ask for help</button>
+                     </th>
+                </table>
+        </div>     
+
+        <!--- Create account form --->
+        <h1 align="center">Don't have an account?</h1>
+        <caption align="center">Don't worry! You can make one right here. :)</caption>
+
+        <hr/>
+
+        <div align="center "style="border: 1px;">
+            <form action="link to the account data base" method="post">
+
+                <label for="username">Username: </label><br>
+                <input type="text" id="username">Type in a username!</input><br>
+
+                <label for="password">Password: </label><br>
+                <input type="text" id="username">Type in a password!</input><br>
+
+                <label for="email">Email: </label><br>
+                <input type="text" id="email">Type in a Email!</input><br>
+
+                <label for="address">Home address: </label><br>
+                <input type="text" id="username"></input><br>
+
+                <label for="phone">Phone Number: </label><br>
+                <input type="text" id="phone"></input><br>
+
+                <input type="submit">Create Account</input><br>
+            </form>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
 
